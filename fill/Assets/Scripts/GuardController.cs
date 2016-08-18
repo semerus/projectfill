2ee988872dcb404fd9f1cb532097025b0a08b684
Yun Joon Soh <yunjoon.soh@gmail.com>
2016-08-19 01:35:33 +0900

    Partially working VG, need to find correct scale, initAngleDelat, tempAngleDelta increase rate

--- fill/Assets/Scripts/GuardController.cs
+++ fill/Assets/Scripts/GuardController.cs
@@ -3,143 +3,219 @@ using System.Collections;
 
 public class GuardController : MonoBehaviour {
 
-	const float fovAngle = 0.00001f;
+	const float initAngleDelta = 0.00001f;
 
 	public GameObject vertexPrefab;
 	GameObject guard;
 	MapGenerator mapGenerator;
 	Vector2[] vertices;
-	int numVertices;
 
-	private Vector3 mousePos;
-	private Vector3 targetPos;
-	private float distance = 10f;
+//	private Vector3 mousePos;
+//	private Vector3 targetPos;
+	private float distance = 30f;
 
-	ArrayList visibleVertices = new ArrayList();
+//	ArrayList visibleVertices = new ArrayList();
 
 
 	// Use this for initialization
 	void Start () {
 		mapGenerator = GameObject.Find("Map Generator").GetComponent<MapGenerator> ();
 		vertices = mapGenerator.vertices;
-		numVertices = vertices.Length - 1;
 
 		guard = Instantiate (vertexPrefab) as GameObject;
 		guard.name = "guard";
-		guard.transform.position = new Vector2 (0f, 0f);
+//		guard.transform.position = new Vector2 (0f, 0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		PositionGuard ();
-		Vector2[] orderedVertices = ShootRays ();
-		renderVG (orderedVertices);
+		Vector3 targetPos = PositionGuard ();
+		Vector2[] orderedVertices = ShootRays (targetPos);
+//		renderVG (orderedVertices);
 	}
 
-	void PositionGuard () {
-		mousePos = Input.mousePosition;
-		targetPos = Camera.main.ScreenToWorldPoint (new Vector3(mousePos.x, mousePos.y, distance));
+	Vector3 PositionGuard () {
+		Vector3 mousePos = Input.mousePosition;
+		Vector3 targetPos = Camera.main.ScreenToWorldPoint (new Vector3(mousePos.x, mousePos.y, distance));
 
 		guard.transform.position = targetPos;
+
+		return targetPos;
 	}
 
-	Vector2[] ShootRays () {
-		Vector2[] orderedVertices = new Vector2[numVertices * 3];
-		
-		for (int i = 0; i < numVertices; i++) {
-			Vector3 standardAngle = vertices [i];
-			Vector3 leftAngle = Quaternion.AngleAxis (-fovAngle, transform.up) * standardAngle;
-			Vector3 rightAngle = Quaternion.AngleAxis (fovAngle, transform.up) * standardAngle;
+	Vector2[] ShootRays (Vector3 targetPos) {
+		Vector2[] orderedVertices = new Vector2[vertices.Length * 3];
 
-			RaycastHit2D leftHit = Physics2D.Linecast (targetPos, leftAngle);
-			RaycastHit2D hit = Physics2D.Linecast (targetPos, vertices [i]);
-			RaycastHit2D rightHit = Physics2D.Linecast (targetPos, rightAngle);
+//		string temp = "";
+//		for (int i = 0; i < vertices.Length - 1; i++) {
+//			temp += vertices[i] + "";
+//		}
+//		Debug.Log (temp);
+//
+//		Debug.Log (temp);
+//		string temp2 = "";
+//		for (int i = 0; i < vertices.Length - 1; i++) {
+////			RaycastHit2D[] hits = Physics2D.LinecastAll (targetPos, vertices [i]);
+////			for (int j = 0; j < hits.Length; j++) {
+////				Debug.Log (Physics.Linecast (targetPos, hits [j].point));
+////				Debug.DrawLine (new Vector2(targetPos.x, targetPos.y), hits [j].point);
+////			}
+//			RaycastHit2D hit = Physics2D.Linecast (targetPos, vertices [i]);
+//			Debug.DrawLine (targetPos, hit.point, Color.red);
+////			Debug.DrawLine (targetPos, vertices[i], Color.white);
+//			temp2 += hit.point;
+//		}
+//		Debug.Log (temp2);
 
-			visibleVertices.Add (leftHit.point);
-			visibleVertices.Add (hit.point);
-			visibleVertices.Add (rightHit.point);
+		for (int i = 0; i < vertices.Length - 1; i++) {
+			int maxCount = 10000;
 
-			Debug.DrawLine (targetPos, leftHit.point, Color.red);
-			Debug.DrawLine (targetPos, hit.point, Color.red);
-			Debug.DrawLine (targetPos, rightHit.point, Color.red);
+			Vector2 standardAngle = (vertices [i] - new Vector2(targetPos.x, targetPos.y)).normalized;
+			Vector2 leftAngle = Quaternion.AngleAxis (initAngleDelta, Vector3.up) * standardAngle;
+			int count = 0;
+			float angleDelta = initAngleDelta;
+			while (leftAngle == standardAngle) {
+				leftAngle = Quaternion.AngleAxis (angleDelta, Vector3.up) * leftAngle;
+				count++;
+				angleDelta *= 2;
+				if (count >= maxCount) {
+					Debug.Log ("LeftAngle passed maxCount");
+					break;
+				}
+			}
+
+			Vector2 rightAngle = Quaternion.AngleAxis (-initAngleDelta, Vector3.up) * leftAngle;
+			count = 0;
+			angleDelta = initAngleDelta;
+			while (rightAngle == standardAngle) {
+				rightAngle = Quaternion.AngleAxis (-angleDelta, Vector3.up) * rightAngle;
+				count++;
+				angleDelta *= 2;
+				if (count >= maxCount) {
+					Debug.Log ("RightAngle passed maxCount");
+					break;
+				}
+			}
+
+			if (leftAngle == standardAngle || rightAngle == standardAngle) {
+				Debug.Log ("Same");
+			}
+
+//			if (leftAngle != rightAngle)
+//				Debug.Log ("diff " + leftAngle + ", " + standardAngle + ", " + rightAngle);
+//			else {
+//				Debug.Log ("same " + leftAngle + ", "  + standardAngle + ", " + rightAngle);
+//			}
+
+//			drawLine (targetPos, standardAngle, Color.red, Color.white);
+//			drawLine (targetPos, leftAngle, Color.red, Color.white);
+//			drawLine (targetPos, rightAngle, Color.red, Color.white);
+
+			drawLine (targetPos, standardAngle);
+			drawLine (targetPos, leftAngle);
+			drawLine (targetPos, rightAngle);
+
+//			if (hit.point == new Vector2 (0, 0)) {
+////				Debug.Log (targetPos + "->" + vertices [i] );
+//				Debug.Log (hit.collider + ": " + targetPos + "->" + vertices[i]);
+//				for (int j = i; j < vertices.Length - 1; j++) {
+//					Debug.DrawLine (targetPos, vertices [j], Color.white);
+//				}
+//			} else {
+//				Debug.Log ("Else: " + hit.collider);
+//			}
+
+
+//			Debug.DrawLine (targetPos, rightHit.point, Color.cyan);
 
 			// add to 2D vertices
-			orderedVertices [i * 3 + 0] = leftHit.point;
-			orderedVertices [i * 3 + 1] = hit.point;
-			orderedVertices [i * 3 + 2] = rightHit.point;
-		}
-
-		for (int i = 0; i < orderedVertices.Length; i++) {
-			Debug.Log (orderedVertices [i]);
+//			orderedVertices [i * 3 + 0] = leftHit.point;
+//			orderedVertices [i * 3 + 1] = hit.point;
+//			orderedVertices [i * 3 + 2] = rightHit.point;
 		}
 
 		return orderedVertices;
 	}
 
-	void renderVG(Vector2[] orderedVertices)
+//	void drawLine (Vector3 targetPos, Vector3 queryAngle, Color c1, Color c2)
+	void drawLine (Vector3 targetPos, Vector3 queryAngle)
 	{
-		for (int i = 0; i < numVertices * 3 - 1; i++) //numVertices * 3 - 1
-		{
-			// TODO: better solution than destroy and re create?
-			Destroy (GameObject.Find ("Triangle" + i));
-			GameObject gObj = new GameObject ("Triangle" + i);
-			Debug.Log ("Creating " + i + "-th triangle");
+		//			RaycastHit2D leftHit = Physics2D.Linecast (targetPos, leftAngle);
+		RaycastHit2D hit = Physics2D.Raycast (targetPos, queryAngle);
+		//			RaycastHit2D rightHit = Physics2D.Linecast (targetPos, rightAngle);
 
-			Vector2[] vertices2D = new Vector2[3];
-			vertices2D [0] = orderedVertices[i];
-			vertices2D [1] = new Vector2(0, 0);
-			vertices2D [2] = orderedVertices[i + 1];
-//			Debug.Log ("Vertices2D[0] " + vertices2D [0] + ", " + vertices2D [1] + ", " + vertices2D [2]);
-
-			renderOneVG (vertices2D, gObj);
-		}
-
-		/* the reason the last triangle is dealt separatly is because of
-		out of bound exception (i.e., using array does not allow wrapping around
-		*/
-
-		// destroy already existing triangle
-		Destroy (GameObject.Find ("FinalTriangle"));
-
-		// create GameObject
-		GameObject gObj_2 = new GameObject("FinalTriangle");
-
-		// create vertices2D info
-		Vector2[] vertices2D_2 = new Vector2[3];
-		vertices2D_2 [0] = orderedVertices[numVertices * 3 - 1];
-		vertices2D_2 [1] = new Vector2(0, 0);
-		vertices2D_2 [2] = orderedVertices[0];
-
-		// render it
-		renderOneVG(vertices2D_2, gObj_2);
+		//			Debug.DrawLine (targetPos, leftHit.point, Color.green);
+		if (hit.collider != null)
+			Debug.DrawLine (targetPos, hit.point, Color.red);
+		else
+			Debug.DrawRay (targetPos, queryAngle, Color.white);
 	}
-
-	void renderOneVG(Vector2[] vertices2D, GameObject gObj)
-	{
-
-		// Create the Vector3 vertices
-		Vector3[] vertices3D = new Vector3[3];
-		vertices3D [0] = new Vector3 (vertices2D[0].x, vertices2D[0].y, 0);
-		vertices3D [1] = new Vector3 (vertices2D[1].x, vertices2D[1].y, 0);
-		vertices3D [2] = new Vector3 (vertices2D[2].x, vertices2D[2].y, 0);
-		//			Debug.Log ("Vertices3D[0] " + vertices3D [0] + ", " + vertices3D [1] + ", " + vertices3D [2]);
-
-		// Use the triangulator to get indices for creating triangles
-		Triangulator tr = new Triangulator (vertices2D);
-		int[] indices = tr.Triangulate ();
-
-		// create polygons
-		MeshRenderer mRend = gObj.AddComponent<MeshRenderer>();
-		MeshFilter filter = gObj.AddComponent<MeshFilter> ();
-
-		mRend.material.color = new Color (1, 0, 0);
-		Mesh msh = new Mesh ();
-		msh.vertices = vertices3D;
-		msh.triangles = indices;
-		msh.RecalculateNormals ();
-		msh.RecalculateBounds ();
-
-		filter.mesh = msh;
-	}
-		
 }
+
+//	void renderVG(Vector2[] orderedVertices)
+//	{
+//		for (int i = 0; i < orderedVertices.Length - 1; i++)
+//		{
+//			// TODO: better solution than destroy and re create?
+//			Destroy (GameObject.Find ("Triangle" + i);
+//			GameObject gObj = new GameObject ("Triangle" + i);
+////			Debug.Log ("Creating " + i + "-th triangle");
+//
+//			Vector2[] vertices2D = new Vector2[3];
+//			vertices2D [0] = orderedVertices[i];
+//			vertices2D [1] = guard.transform.position;
+//			vertices2D [2] = orderedVertices[i + 1];
+//			Debug.Log ("Vertices2D[0] " + vertices2D [0] + ", " + vertices2D [1] + ", " + vertices2D [2]);
+//
+//			renderOneVG (vertices2D, gObj);
+//		}
+//
+//		/* the reason the last triangle is dealt separatly is because of
+//		out of bound exception (i.e., using array does not allow wrapping around
+//		*/
+//
+//		// destroy already existing triangle
+//		Destroy (GameObject.Find ("FinalTriangle"));
+//
+//		// create GameObject
+//		GameObject gObj_2 = new GameObject("FinalTriangle");
+//
+//		// create vertices2D info
+//		Vector2[] vertices2D_2 = new Vector2[3];
+//		vertices2D_2 [0] = orderedVertices[orderedVertices.Length - 1];
+//		vertices2D_2 [1] = guard.transform.position;
+//		vertices2D_2 [2] = orderedVertices[0];
+//
+//		// render it
+//		renderOneVG(vertices2D_2, gObj_2);
+//	}
+//
+//	void renderOneVG(Vector2[] vertices2D, GameObject gObj)
+//	{
+//
+//		// Create the Vector3 vertices
+//		Vector3[] vertices3D = new Vector3[3];
+//		vertices3D [0] = new Vector3 (vertices2D[0].x, vertices2D[0].y, 0);
+//		vertices3D [1] = new Vector3 (vertices2D[1].x, vertices2D[1].y, 0);
+//		vertices3D [2] = new Vector3 (vertices2D[2].x, vertices2D[2].y, 0);
+//		//			Debug.Log ("Vertices3D[0] " + vertices3D [0] + ", " + vertices3D [1] + ", " + vertices3D [2]);
+//
+//		// Use the triangulator to get indices for creating triangles
+//		Triangulator tr = new Triangulator (vertices2D);
+//		int[] indices = tr.Triangulate ();
+//
+//		// create polygons
+//		MeshRenderer mRend = gObj.AddComponent<MeshRenderer>();
+//		MeshFilter filter = gObj.AddComponent<MeshFilter> ();
+//
+//		mRend.material.color = new Color (1, 0, 0);
+//		Mesh msh = new Mesh ();
+//		msh.vertices = vertices3D;
+//		msh.triangles = indices;
+//		msh.RecalculateNormals ();
+//		msh.RecalculateBounds ();
+//
+//		filter.mesh = msh;
+//	}
+//		
+//}
--- fill/Assets/Scripts/MapGenerator.cs
+++ fill/Assets/Scripts/MapGenerator.cs
@@ -6,64 +6,79 @@ public class MapGenerator : MonoBehaviour {
 	public GameObject linePrefab;
 	public GameObject vertexPrefab;
 
-
-	//GameObject line;
-	Vector2 p0 = new Vector2 (0f, -7f);
-	Vector2 p1 = new Vector2 (8f, -10f);
-	Vector2 p2 = new Vector2 (3f, -3f);
-	Vector2 p3 = new Vector2 (8f, 5f);
-	Vector2 p4 = new Vector2 (3f, 3f);
-	Vector2 p5 = new Vector2 (0f, 10f);
-	Vector2 p6 = new Vector2 (-3f, 3f);
-	Vector2 p7 = new Vector2 (-8f, 5f);
-	Vector2 p8 = new Vector2 (-3f, -3f);
-	Vector2 p9 = new Vector2 (-8f, -10f);
-
 	public Vector2[] vertices;
-	public int length;
+
+	int scale = 1000;
 
 	// Use this for initialization
 	void Start () {
+		Camera.main.orthographicSize *= scale; 
+
+		//GameObject line;
+		Vector2 p0 = new Vector2 (0f, -7f) * scale;
+		Vector2 p1 = new Vector2 (8f, -10f) * scale;
+		Vector2 p2 = new Vector2 (3f, -3f) * scale;
+		Vector2 p3 = new Vector2 (8f, 5f) * scale;
+		Vector2 p4 = new Vector2 (3f, 3f) * scale;
+		Vector2 p5 = new Vector2 (0f, 10f) * scale;
+		Vector2 p6 = new Vector2 (-3f, 3f) * scale;
+		Vector2 p7 = new Vector2 (-8f, 5f) * scale;
+		Vector2 p8 = new Vector2 (-3f, -3f) * scale;
+		Vector2 p9 = new Vector2 (-8f, -10f) * scale;
+
+		//	Vector2 p0 = new Vector2 (-5f, 5f);
+		//	Vector2 p1 = new Vector2 (-5f, -5f);
+		//	Vector2 p2 = new Vector2 (2f, 3f);
+		//	Vector2 p3 = new Vector2 (5f, 5f);
+
 		vertices = new Vector2[] {p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p0};
-		length = vertices.Length - 1;
-		CreateMap ();
+//		vertices = new Vector2[] {p0, p1, p2, p3, p0};
+//		vertices = new Vector2[] {p0, p1, p2};
+
+		CreateMap (vertices);
 	}
 
 	//creating map using the vertices
-	void CreateMap () {
-		for (int i = 0; i < length; i++) {
-			GameObject temp = Instantiate (linePrefab) as GameObject;
-			temp.name = "line"+i;
-			LineRenderer lineRenderer = temp.GetComponent<LineRenderer> ();
-			Vector3[] positions = new Vector3[2];
-			if (i < length - 1) {
-				positions [0] = vertices [i];
-				positions [1] = vertices [i + 1];
-			} else {
-				positions [0] = vertices [i];
-				positions [1] = vertices [0];
-			}
-			lineRenderer.SetPositions(positions);
+	void CreateMap (Vector2[] vertices) {
+		// assert there are at least 3 vertices
+		if (vertices.Length < 3) {
+			Debug.LogError ("not enough vertices");
 		}
-		CreateVertex ();
-		CreateCollider ();
+
+		GameObject temp = new GameObject("Line Renderer");
+//		GameObject temp = Instantiate (linePrefab) as GameObject;
+		LineRenderer lineRenderer = temp.AddComponent<LineRenderer> ();
+		lineRenderer.SetWidth (0.1f * scale, 0.1f * scale);
+		lineRenderer.SetColors (Color.white, Color.black);
+		lineRenderer.SetVertexCount (vertices.Length);
+
+		// add vertices
+		for (int i = 0; i < vertices.Length; i++) {
+			lineRenderer.SetPosition (i, vertices[i]);
+		}
+
+		// add last vertex
+//		lineRenderer.SetPosition (vertices.Length, vertices[0]);
+
+//		CreateVertex (vertices);
+		CreateEdgeCollider ();
 	}
 
-	//creating a dot sprite for each vertex to render round corners
-	void CreateVertex() {
-		for (int i = 0; i < length; i++) {
-			GameObject vertex = Instantiate (vertexPrefab) as GameObject;
-			vertex.name = "vertex" + i;
-			vertex.transform.position = vertices [i];
-		}
-	}
+//	//creating a dot sprite for each vertex to render round corners
+//	void CreateVertex(Vector2[] vertices) {
+//		
+//		for (int i = 0; i < vertices.Length - 1; i++) {
+//			GameObject vertex = Instantiate (vertexPrefab) as GameObject;
+//			vertex.name = "vertex" + i;
+//			vertex.transform.position = vertices [i];
+//		}
+//	}
 
 	//creating EdgeCollider2D by connecting the vertices of the polygon
-	void CreateCollider () {
+	void CreateEdgeCollider () {
 		GameObject edge = new GameObject ("Edge Collider");
-		edge.AddComponent<EdgeCollider2D> ();
-		EdgeCollider2D edgeCollider = edge.GetComponent<EdgeCollider2D> ();
 
+		EdgeCollider2D edgeCollider = edge.AddComponent<EdgeCollider2D> ();
 		edgeCollider.points = vertices;
 	}
 }
