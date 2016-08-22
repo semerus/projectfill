9cab15338796bd3ac70da2d63f9505beb3d4604d
semerus <semerus4u@gmail.com>
2016-08-22 12:35:19 +0900

    adding new project folder, only the reading json file is done

---
+++ MapEditor/Assets/Scripts/MapDrawer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapDrawer : MonoBehaviour {
+	public Vector2[] vertices;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DrawLine ();
+	}
+
+	void DrawLine () {
+		Vector2 startLine;
+		Vector2 endLine;
+		bool drawing = false;
+		if (Input.GetMouseButtonUp (0)) {
+			drawing = true;
+		}
+	}
+}
---
+++ MapEditor/Assets/Scripts/ReadJson.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using LitJson;
+
+public class ReadJson : MonoBehaviour {
+    private string path = "/Resources/Maps.json";
+	private int stage = 1;
+
+	public MapData mapData;
+
+	void Start () {
+		mapData = ReadMapVertices (path, stage);
+		Debug.Log (mapData.vertices[1]);
+	}
+
+	MapData ReadMapVertices(string path, int stage) {
+		string jsonString = File.ReadAllText (Application.dataPath + path);
+		JsonData data = JsonMapper.ToObject (jsonString);
+		Vector2[] vertices = new Vector2[data ["Maps"] [stage - 1] ["vertices"].Count];
+		for (int i = 0; i < data ["Maps"] [stage - 1] ["vertices"].Count; i++) {
+			double x = (double) data ["Maps"] [stage - 1] ["vertices"] [i] ["x"];
+			double y = (double)data ["Maps"] [stage - 1] ["vertices"] [i] ["y"];
+			vertices [i].x = (float) x;
+			vertices [i].y = (float) y; 
+		}
+		MapData mapData = new MapData((int)data ["Maps"] [stage - 1] ["id"], data ["Maps"] [stage - 1] ["name"].ToString (), vertices);
+		return mapData;
+    }
+
+	public class MapData {
+		public int id;
+		public string name;
+		public Vector2[] vertices;
+
+		public MapData(int id, string name, Vector2[] vertices){
+			this.id = id;
+			this.name = name;
+			this.vertices = vertices;
+		}
+	}
+}
