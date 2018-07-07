264cf14c904ac06c18ad952a83246ab92065be4c
semerus <semerus4u@gmail.com>
2018-07-07 17:45:50 +0900

    made playbutton not interactable

--- fill/Assets/Scripts/UI/MainMenuScene.cs
+++ fill/Assets/Scripts/UI/MainMenuScene.cs
@@ -32,41 +32,14 @@ namespace FillClient.UI
         void FirstInit()
         {
             if (isInitialized) { return; }
-            root = GameObject.Find("Root");
+			SetButtonListeners ();
+
 			startButton = root.FindChildByName ("StartButton").GetComponent<Button>();
 			touchText = startButton.GetComponentInChildren<Text> ();
-			playButton = root.FindChildByName("PlayButton").GetComponent<Button>();
-            editorButton = root.FindChildByName("EditorButton").GetComponent<Button>();
-            customButton = root.FindChildByName("CustomButton").GetComponent<Button>();
-
 			startButton.onClick.AddListener (() => 
 			{
 				ShiftButtons();	
 			});
-
-            playButton.onClick.AddListener(() =>
-            {
-                new SwitchStateMessage()
-                {
-					NextState = FillState.StageSelect,
-                }.Dispatch();
-            });
-
-            editorButton.onClick.AddListener(() =>
-            {
-                new SwitchStateMessage()
-                {
-                    NextState = FillState.StageMaker,
-                }.Dispatch();
-            });
-
-            customButton.onClick.AddListener(() =>
-            {
-                new SwitchStateMessage()
-                {
-                    NextState = FillState.CustomStageSelect,
-                }.Dispatch();
-            });
 
 			var touchAnim = touchText.DOFade(0f, 1f).SetLoops(-1, LoopType.Yoyo);
 
@@ -76,12 +49,31 @@ namespace FillClient.UI
 		void Init()
 		{
 			if (isInitialized) { return; }
+			SetButtonListeners ();
 
-			root = GameObject.Find("Root");
 			startButton = root.FindChildByName ("StartButton").GetComponent<Button>();
+			startButton.gameObject.SetActive (false);
+
+			var logo = root.FindChildByName ("Logo").GetComponent<RectTransform>();
+			var startPos = logo.anchoredPosition;
+			logo.anchoredPosition = new Vector2 (350f, startPos.y);
+			//logo.DOAnchorPos(new Vector2(350f, startPos.y), 1.6f);
+
+			var menuAlpha = root.FindChildByName ("LeftMenuBar").GetComponent<AlphaGroup> ();
+			menuAlpha.GroupAlpha = 1f;
+
+			isInitialized = true;
+		}
+
+		void SetButtonListeners()
+		{
+			root = GameObject.Find("Root");
 			playButton = root.FindChildByName("PlayButton").GetComponent<Button>();
 			editorButton = root.FindChildByName("EditorButton").GetComponent<Button>();
 			customButton = root.FindChildByName("CustomButton").GetComponent<Button>();
+
+			// 아직 기능이 없어서 비활성화
+			playButton.interactable = false;
 
 			playButton.onClick.AddListener(() =>
 				{
@@ -106,18 +98,6 @@ namespace FillClient.UI
 						NextState = FillState.CustomStageSelect,
 					}.Dispatch();
 				});
-
-			startButton.gameObject.SetActive (false);
-
-			var logo = root.FindChildByName ("Logo").GetComponent<RectTransform>();
-			var startPos = logo.anchoredPosition;
-			logo.anchoredPosition = new Vector2 (350f, startPos.y);
-			//logo.DOAnchorPos(new Vector2(350f, startPos.y), 1.6f);
-
-			var menuAlpha = root.FindChildByName ("LeftMenuBar").GetComponent<AlphaGroup> ();
-			menuAlpha.GroupAlpha = 1f;
-
-			isInitialized = true;
 		}
 
 		void ShiftButtons()
